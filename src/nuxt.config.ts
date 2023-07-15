e0dd7125b812f54ed4508d45a3c3c50203b3dd61
tomoki-ls69-utl1-5 <stbwonder0725@g.ecc.u-tokyo.ac.jp>
2023-07-15 19:32:49 +0900

    グローバルscss追加、nuxt.configに追記

--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -1,4 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  components: [{ path: "~/components", pathPrefix: false }],
+  buildModules: ["@nuxtjs/style-resources"],
+  css: ["@/assets/scss/common.scss", "@/assets/scss/helpers.scss"],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "@/assets/scss/_variables.scss";',
+        },
+      },
+    },
+  },
+  typescript: {
+    shim: false,
+    strict: true,
+    typeCheck: false,
+  },
 });
