8bf1ddeb4d49a4929756d50ada4b46cbb3956a7e
tomoki-ls69-utl1-5 <stbwonder0725@g.ecc.u-tokyo.ac.jp>
2023-07-21 00:08:25 +0900

    複数データの読み込みまでできた

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,12 @@
 <template lang="pug">
-p {{ data }}
+div
+  //- p {{ pastGaisyuData }}
+  //- p {{ studentDiscountData }}
 </template>
 
 <script setup lang="ts">
-  const thisPlaceAssignData = await useFetch('/api/thisPlaceAssign', {key: 'thisPlaceAssignData'})
+  const { data: thisPlaceAssignData } = await useFetch('/api/thisPlaceAssign', {key: 'thisPlaceAssignData'})
+  const { data: pastPlaceAssignData } = await useFetch('/api/pastPlaceAssign.get', {key: 'pastPlaceAssignData'})
+  const { data: studentDiscountData } = await useFetch('/api/studentDiscount', {key: 'studentDiscountData'})
+  const { data: pastGaisyuData } = await useFetch('/api/pastGaisyu', {key: 'pastGaisyuData'})
 </script>
