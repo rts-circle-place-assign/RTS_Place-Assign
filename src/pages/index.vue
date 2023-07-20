1c87caa46b022487ae96b33c5d1c30c334bfdd0a
tomoki-ls69-utl1-5 <stbwonder0725@g.ecc.u-tokyo.ac.jp>
2023-07-21 02:47:05 +0900

    コンポーネントを使ってみた

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,14 @@
 <template>
 <o-article-template title="博麗神社例大祭　配置関連web">
-
+  <a-link-button link="https://zenn.dev/t_keshi/scraps/9ddb388bc6975d"></a-link-button>
+  <m-article-window head="見出し1">
+    <template #head>
+      <m-article-paragraph secondhead="見出し2">
+        <template #secondhead>
+          <p>あああ</p>
+        </template>
+      </m-article-paragraph>
+    </template>
+  </m-article-window>
 </o-article-template>
 </template>
