18208348632eaefa138d7827ca035eebe81d1622
tomoki-ls69-utl1-5 <stbwonder0725@g.ecc.u-tokyo.ac.jp>
2023-07-21 02:46:22 +0900

    大見出し追加

---
+++ src/components/atoms/AArticleHead.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+interface Props {
+  head: string
+  mayFont: boolean
+  fontSize: number
+}
+
+withDefaults(defineProps<Props>(), {
+  head: "",
+  mayFont: false,
+  fontSize: 20,
+})
+</script>
+
+<template>
+  <div class="aheadwindow-wrapper flex">
+    <img
+      src="../../assets/img/template/h2_leaves.svg"
+      class="align-self-start h-20 ouchi"
+    />
+    <h2
+      class="midashi my-0 lh-short"
+      :class="{ mayFont: mayFont }"
+      :style="{ fontSize: fontSize + 'px !important' }"
+    >
+      {{ head }}
+    </h2>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.aheadwindow-wrapper {
+  margin-top: 40px;
+}
+.midashi {
+  color: $main-dark;
+}
+.article-title {
+  position: relative;
+  z-index: 1;
+  width: fit-content;
+  color: $text1;
+  padding: 0px 0px 0px 15px;
+  font-size: 36px;
+}
+.align-self-start {
+  align-self: flex-start;
+}
+.lh-short {
+  line-height: 1.5em;
+}
+.mayFont {
+  font-family: 'anzu', sans-serif !important;
+  font-size: 140% !important;
+}
+.ouchi {
+  margin-top: -7px;
+}
+</style>
---
+++ src/components/molecules/MArticleWindow.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+interface Props {
+  head?: string
+  mayFont?: boolean
+  fontSize?: number
+}
+withDefaults(defineProps<Props>(), {
+  head: "",
+  mayFont: false,
+  fontSize: 20
+})
+</script>
+
+<template>
+  <div class="m-article-window">
+    <a-article-head
+      v-show="head"
+      :head="head"
+      :mayFont="mayFont"
+      :fontSize="fontSize"
+    />
+    <br v-show="!head" />
+    <div class="slot slot-1">
+      <slot name="head" />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.slot {
+  margin-left: 20px;
+  width: 80vw;
+  @include mq(smallpc) {
+    margin-left: 1.5rem;
+    width: 80vw;
+  }
+  @media screen and (min-width: 1130px) {
+    // pc版
+    width: 980px;
+  }
+  @media screen and (min-width: 1600px) {
+    width: 1000px;
+  }
+  &-1 {
+    margin-bottom: 10px;
+  }
+}
+.komidashi {
+  margin-left: 15px;
+  font-weight: bold !important;
+}
+</style>
