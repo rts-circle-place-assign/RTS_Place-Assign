09763b879caefefc7efe0c33ccc91964ca3125ff
tomoki-ls69-utl1-5 <stbwonder0725@g.ecc.u-tokyo.ac.jp>
2023-07-26 03:17:14 +0900

    企画検索ボックス

---
+++ src/components/atoms/ASearchBox.vue
@@ -0,0 +1,133 @@
+<script setup lang="ts">
+import { Circle } from '../../lib/hooks/kikaku'
+import { useFuse } from '@vueuse/integrations/useFuse'
+import { useKikakuStore } from '../../composables/useKikakuStore';
+import { useSearchWordStore } from '../../composables/useSearchWordStore';
+
+interface Props {
+  defaultword?: string
+  barcolor?: string
+  pcyoko?: string
+  spyoko?: string
+  tate?: number
+  buttonkado?: string
+  barkado?: string
+  keyword?: string
+}
+const props = withDefaults(defineProps<Props>(), {
+  defaultword: '',
+  barcolor: '',
+  pcyoko: '60%',
+  spyoko: '80%',
+  tate: 40,
+  buttonkado: '20px 0px 0px 20px',
+  barkado: '0px 20px 20px 0px',
+  keyword: ''
+})
+
+const options = {
+  fuseOptions: {
+    threshold: 0.3,
+    keys: ['circlename', 'circlenamekana', 'penname', 'pennamekana'],
+  }
+}
+
+const word = ref('')
+const { data: thisPlaceAssignData } = await useFetch('/api/thisPlaceAssign', {key: 'thisPlaceAssignData'})
+
+const searchWordStore = useSearchWordStore()
+const { state: searchWordState, setWord } = searchWordStore
+const kikakuStore = useKikakuStore()
+const { state, setKikaku } = kikakuStore
+const router = useRouter()
+watch(word, (key, _prevkey) => {
+  setWord(key)
+})
+const runSearch = async () => {
+  const ddd = thisPlaceAssignData as Circle[]
+  const { results } = useFuse(word, ddd, options)
+  const resultArr = results.value.map(i => i.item)
+  setKikaku(resultArr)
+  router.push('/search')
+}
+</script>
+
+<template>
+  <div
+    class="search-box flex align-center"
+    :style="{ '--pcyoko': pcyoko, '--spyoko': spyoko, height: tate + 'px' }"
+  >
+    <!-- <span class="flex justify-center">
+      <img
+        src="../../assets/img/top/search_main_dull.svg"
+        :style="{ width: tate / 2.5 + 'px' }"
+      />
+    </span> -->
+    <button
+      class="search-button flex align-center justify-center cursor-pointer full-height"
+      :style="{
+        'background-color': barcolor,
+        '--buttonkado': buttonkado,
+        width: tate + 'px',
+      }"
+      @click="runSearch"
+    >
+      <span class="flex justify-center">
+        <img
+          src="../../assets/img/top/search_main_dull.svg"
+          :style="{ width: tate / 2.5 + 'px' }"
+        />
+      </span>
+    </button>
+    <form
+      class="search-text-box full-height"
+      role="search"
+      @submit.prevent="runSearch"
+    >
+      <input
+        v-model="word"
+        class="search-text fluid full-height"
+        :style="{ 'background-color': barcolor, '--barkado': barkado }"
+        type="search"
+        :placeholder="defaultword"
+        autocomplete="on"
+      />
+    </form>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.search-box {
+  width: var(--pcyoko);
+  @include mq(sp) {
+    width: var(--spyoko);
+  }
+}
+.search-text-box {
+  width: calc(100% - 40px);
+}
+.search-text {
+  border: none;
+  outline: none;
+  font-size: 1em;
+  background: none;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  border-radius: var(--barkado);
+  padding-top: 5px;
+  padding-bottom: 5px;
+  outline: none !important;
+  font-family: 'Zen Maru Gothic', sans-serif !important;
+}
+[type='search']::-webkit-search-cancel-button,
+[type='search']::-webkit-search-decoration {
+  -webkit-appearance: none;
+}
+.search-button {
+  border: none;
+  border-radius: var(--buttonkado);
+  outline: none;
+  font-size: 16px;
+}
+</style>
