11f18c86117758363fdfdc4cbd5a32c04a62719e
tomoki-ls69-utl1-5 <stbwonder0725@g.ecc.u-tokyo.ac.jp>
2023-07-26 03:18:10 +0900

    企画お気に入り機能

---
+++ src/components/atoms/AKikakuCardFav.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { useStorage } from '@vueuse/core'
+
+interface Props {
+  id: number
+}
+const props = defineProps<Props>()
+const { kaikiEn } = useKaikiStore()
+const favListName = computed(() => kaikiEn.value.toLowerCase() + 'fav-list')
+const favList = useStorage(favListName.value, [] as number[])
+const liked = computed(() => {
+  // @ts-ignore
+  return favList.value.includes(props.id)
+})
+const icon = computed(() => {
+  return [liked.value ? 'bookmark.svg' : 'bookmark_white.svg' ?? 'bookmark.svg']
+})
+const fav = () => {
+  if (liked.value) {
+    // @ts-ignore
+    favList.value = favList.value.filter((i: number) => i !== props.id)
+  } else {
+    // @ts-ignore
+    favList.value.push(props.id)
+  }
+}
+</script>
+
+<template>
+  <div class="icon-wrapper" @click="fav">
+    <img class="fav-dao" :src="`/img/search/${icon}`" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.icon-wrapper {
+  font-size: 1.5rem;
+  height: 55px;
+  width: 43px;
+}
+.fav-dao {
+  position: relative;
+  top: 15px;
+  left: 13px;
+  width: 31.5px;
+}
+</style>
