2762cf8489eb151cbebde224fb0dc22f8712d36b
tomoki-ls69-utl1-5 <stbwonder0725@g.ecc.u-tokyo.ac.jp>
2023-07-26 03:23:11 +0900

    その他データ取得の記述をコメントアウト

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,9 +4,9 @@ useHead({
 })
 
 const { data: thisPlaceAssignData } = await useFetch('/api/thisPlaceAssign', {key: 'thisPlaceAssignData'})
-const { data: pastPlaceAssignData } = await useFetch('/api/pastPlaceAssign.get', {key: 'pastPlaceAssignData'})
-const { data: studentDiscountData } = await useFetch('/api/studentDiscount', {key: 'studentDiscountData'})
-const { data: pastGaisyuData } = await useFetch('/api/pastGaisyu', {key: 'pastGaisyuData'})
+// const { data: pastPlaceAssignData } = await useFetch('/api/pastPlaceAssign.get', {key: 'pastPlaceAssignData'})
+// const { data: studentDiscountData } = await useFetch('/api/studentDiscount', {key: 'studentDiscountData'})
+// const { data: pastGaisyuData } = await useFetch('/api/pastGaisyu', {key: 'pastGaisyuData'})
 </script>
 
 <template>
