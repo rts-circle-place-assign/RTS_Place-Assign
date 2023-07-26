a21b4a72403891a3fc37e5b379f263f5c3d4dabb
tomoki-ls69-utl1-5 <stbwonder0725@g.ecc.u-tokyo.ac.jp>
2023-07-27 02:18:38 +0900

    そもそもデータをfetchできているのかチェック

---
+++ src/pages/test.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const { data: thisPlaceAssignData } = await useFetch('/api/thisPlaceAssign', {
+  key: 'thisPlaceAssignData',
+  // onResponse({ request, response, options }) {
+  //   setKikaku(shuffle(response._data).slice(0, 12))
+  // },
+})
+</script>
+
+<template>
+  <p>{{ thisPlaceAssignData }}</p>
+</template>
