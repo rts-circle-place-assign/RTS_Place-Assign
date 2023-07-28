e6b7c0ee7c708f4a4b6c5a18b2e23b0dc80f39b3
tomoki-ls69-utl1-5 <stbwonder0725@g.ecc.u-tokyo.ac.jp>
2023-07-29 01:03:29 +0900

    console削除

--- src/server/api/thisPlaceAssign.get.ts
+++ src/server/api/thisPlaceAssign.get.ts
@@ -4,7 +4,5 @@ const config = useRuntimeConfig()
 
 export default defineEventHandler(async () => {
   const result: Circle[] = await $fetch(config.public.thisPlaceAssign)
-  console.log('got data!')
-  // console.log(result)
   return result
 })
