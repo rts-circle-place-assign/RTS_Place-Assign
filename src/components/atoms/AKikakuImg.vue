aa89adb710aa19109c2cb29d22afe51c801701a0
tomoki-ls69-utl1-5 <stbwonder0725@g.ecc.u-tokyo.ac.jp>
2023-07-30 01:54:30 +0900

    企画詳細ページ内のサクカ（ワイドビュー）画像

---
+++ src/components/atoms/AKikakuImg.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import { useMouse } from '@vueuse/core'
+import { Circle, cutURL } from '../../lib/hooks'
+interface Props {
+  kikaku: Circle
+}
+const props = defineProps<Props>()
+const imgUrl = cutURL(props.kikaku)
+</script>
+
+<template>
+  <div class="mb--20">
+    <div
+      class="hi-gazo bokashi"
+      :style="{ 'background-image': `url(${imgUrl})` }"
+    >
+      <div class="kikaku-image-container text-align-center">
+        <img class="aweye" :src="imgUrl" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.hi-gazo {
+  background-size: 100vw;
+  background-position: center;
+  height: 140px;
+  @include mq(from-tab) {
+    height: 228px;
+  }
+}
+.bokashi {
+  position: relative;
+  z-index: 0;
+  &::before {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    bottom: 0px;
+    right: 0px;
+    background: inherit;
+    filter: blur(2px) brightness(85%);
+    content: '';
+    z-index: -1;
+  }
+}
+.aweye {
+  // max-width: 212px;
+  height: 140px;
+  @include mq(from-tab) {
+    margin-top: 68px;
+  }
+}
+</style>
