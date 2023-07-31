9a9dd23d2eaa059ae67aca6be382e4e3d84a83b5
tomoki-ls69-utl1-5 <stbwonder0725@g.ecc.u-tokyo.ac.jp>
2023-08-01 02:11:16 +0900

    小見出しのコンポーネント

---
+++ src/components/atoms/AArticleSmallHeading.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+interface Props {
+  title: string
+}
+defineProps<Props>()
+</script>
+
+<template>
+  <p class="a-article-small-heading pl-12 my-8">
+    {{ title }}
+  </p>
+</template>
+
+<style lang="scss" scoped>
+.a-article-small-heading {
+  color: $main-dark;
+  font-weight: bold;
+  position: relative;
+  &::before {
+    position: absolute;
+    width: 0.3rem;
+    height: 14px;
+    left: 0;
+    top: 50%;
+    transform: translateY(-50%);
+    content: '';
+    background-color: $main-dark;
+    @include mq(until-tab) {
+      height: calc(100% - 4px);
+      left: 1px;
+    }
+  }
+}
+</style>
