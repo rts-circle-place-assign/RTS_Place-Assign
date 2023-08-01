c2c31b94e9744a52ca419f04de333d55197bcc33
tomoki-ls69-utl1-5 <stbwonder0725@g.ecc.u-tokyo.ac.jp>
2023-08-01 09:35:13 +0900

    合体サークル間比較ページのサークルカットコンポーネント

---
+++ src/components/atoms/ACircleCut.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { Circle, cutURL } from '../../lib/hooks/'
+
+interface Props {
+  kikaku: Circle
+}
+defineProps<Props>()
+
+const popupOpen = ref(false)
+const setPopupOpen = (state: boolean) => (popupOpen.value = state)
+</script>
+
+<template>
+  <div>
+    <img
+      :src="cutURL(kikaku)"
+      :alt="kikaku.circlename + 'のサークルカット'"
+      class="thumbnail cursor-zoomin"
+      @click="setPopupOpen(true)"
+    />
+    <a-popup :isOpen="popupOpen" @close="setPopupOpen(false)">
+      <img
+        :src="cutURL(kikaku)"
+        :alt="kikaku.circlename + 'の拡大版サークルカット'"
+      />
+    </a-popup>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.thumbnail {
+  // max-height: 120px;
+  max-width: 100%;
+  // max-width: 150px;
+  // @include mq(from-tab) {
+  //   max-width: 400px;
+  // }
+  // height: auto;
+}
+</style>
