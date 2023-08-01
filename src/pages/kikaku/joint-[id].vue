c9e97ac802801e8f4d4347dae5e904f81eff5f13
tomoki-ls69-utl1-5 <stbwonder0725@g.ecc.u-tokyo.ac.jp>
2023-08-01 09:35:54 +0900

    合体サークル間比較のページ

---
+++ src/pages/kikaku/joint-[id].vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { storeToRefs } from 'pinia'
+import { useKikakuCardById } from '../../lib/hooks'
+import { useKikakuAllStore } from '~/store/'
+
+const store = useKikakuAllStore()
+const { kikakuAll } = storeToRefs(store)
+const route = useRoute()
+const kikaku = useKikakuCardById(kikakuAll.value, Number(route.params.id))
+useHead({
+  title: kikaku?.circlename,
+})
+</script>
+
+<template>
+  <div v-if="kikaku">
+    <a-kikaku-img :kikaku="kikaku" />
+    <div>
+      <o-kikaku-title :kikaku="kikaku" />
+      <div class="kabu">
+        <t-joint-circles :kikaku="kikaku" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.kabu {
+  width: 80vw;
+  max-width: 1000px;
+  margin: auto;
+}
+</style>
