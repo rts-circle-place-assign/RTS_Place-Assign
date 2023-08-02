e967197f7eac3949c485294026c44c48e9de392a
tomoki-ls69-utl1-5 <stbwonder0725@g.ecc.u-tokyo.ac.jp>
2023-08-02 14:26:20 +0900

    修正流れの作業のコンポーネント

---
+++ src/components/atoms/ATimelineBox.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+interface Props {
+  data?: string
+  head?: string
+  isBold?: boolean
+  dateTime?: string
+  important?: boolean
+  link?: string
+}
+const props = withDefaults(defineProps<Props>(), {
+  data: '',
+  head: '',
+  isBold: false,
+  link: '',
+  dateTime: '',
+})
+
+const componentName = computed(() =>
+  props.link ? resolveComponent('nuxt-link') : 'span'
+)
+</script>
+
+<template>
+  <div class="timeline-item">
+    <!-- <div class="date">
+      <p :class="[{ bold: isBold }, { 'red bold': important }]">
+        {{ dateTime }}
+      </p>
+    </div> -->
+    <div class="content">
+      <component :is="componentName" :to="link" class="link">{{
+        head
+      }}</component>
+      <slot name="head" />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.timeline-item {
+  padding: 0.5rem 0 1rem 1rem;
+  position: relative;
+  // color: rgba(black, .7);
+  border-left: 2px solid $black;
+  font-size: 1.2rem;
+  & .date {
+    font-size: 1rem;
+    margin-bottom: -0.3rem;
+  }
+
+  &::after {
+    width: 10px;
+    height: 10px;
+    display: block;
+    top: 1em;
+    position: absolute;
+    left: -8px;
+    border-radius: 10px;
+    content: '';
+    border: 2px solid $black;
+    background: white;
+  }
+
+  &:last-child {
+    border-image: linear-gradient(to bottom, $black 60%, rgba($black, 0)) 1 100%;
+  }
+}
+.link {
+  text-decoration: none;
+}
+</style>
---
+++ src/components/molecules/MFixFlow.vue
@@ -0,0 +1,77 @@
+<script setup lang="ts">
+const pages = [
+  {
+    title: '修正前作業',
+    link: 'before',
+  },
+  {
+    title: '学割チェック',
+    link: 'student-discount',
+  },
+  {
+    title: 'なかよしコード特殊処理',
+    link: 'friend-code-particular',
+  },
+  {
+    title: '2サークルなかよしコード処理',
+    link: 'two-circles-friend-code',
+  },
+  {
+    title: 'なかよしコード・合体矛盾処理（成年向け有無編）',
+    link: 'joint-adult-different',
+  },
+  {
+    title: 'なかよしコード・合体矛盾処理（メディアコード編）',
+    link: 'joint-media-different',
+  },
+  {
+    title: 'なかよしコード・合体矛盾処理（作品コード編）',
+    link: 'joint-sakuhin-different',
+  },
+  {
+    title: 'コスプレメディアコード修正',
+    link: 'cosplay-correct',
+  },
+  {
+    title: '作品コード・キャラ矛盾処理',
+    link: 'sakuhin-media-different',
+  },
+  {
+    title: 'グッズ矛盾処理',
+    link: 'goods-different',
+  },
+  {
+    title: 'メディアコード「その他」処理',
+    link: 'media-other',
+  },
+  {
+    title: 'サークルカット規格チェック',
+    link: 'cut-check',
+  },
+]
+</script>
+
+<template>
+  <div class="container">
+    <a-timeline-box
+      v-for="(page, i) in pages"
+      :key="i"
+      :head="page.title"
+      :link="'/fix/' + page.link"
+    ></a-timeline-box>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  max-width: 1024px;
+  width: 100%;
+  @include mq(from-pc) {
+    margin: 0 auto;
+  }
+  margin: 0 auto;
+  @include mq(until-tab) {
+    margin-left: 0.5rem;
+  }
+}
+</style>
