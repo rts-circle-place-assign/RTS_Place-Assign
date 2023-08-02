5e37bb5b6e234db07dd6d2c5e898dea477cc050a
tomoki-ls69-utl1-5 <stbwonder0725@g.ecc.u-tokyo.ac.jp>
2023-08-02 14:25:13 +0900

    修正作業のindexページを作成

---
+++ src/pages/fix/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+useHead({
+  title: 'サークル申込データ修正',
+})
+</script>
+
+<template>
+  <o-article-template title="サークル申込データ修正">
+    <div class="flex justify-center mx-auto">
+      <a-link-button class="mb-20" link="/fix/result"
+        >修正結果集計</a-link-button
+      >
+    </div>
+    <m-fix-flow />
+  </o-article-template>
+</template>
