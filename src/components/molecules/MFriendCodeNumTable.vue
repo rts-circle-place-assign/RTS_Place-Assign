fba1bfc0ac6824d090b280eb89a2d5b23086ac9a
tomoki-ls69-utl1-5 <stbwonder0725@g.ecc.u-tokyo.ac.jp>
2023-08-02 14:27:51 +0900

    2サークルなかよしコード処理のページ

---
+++ src/components/molecules/MFriendCodeNumTable.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { storeToRefs } from 'pinia'
+import { Circle, useCircles } from '../../lib/hooks'
+import { useKikakuAllStore } from '../../store/'
+
+const store = useKikakuAllStore()
+const { kikakuAll } = storeToRefs(store)
+const allData = kikakuAll.value as Circle[]
+const friendsCircle = useCircles(allData, 'friendCode')
+</script>
+
+<template>
+  <table class="check-table mt-20">
+    <thead>
+      <tr>
+        <th>なかよしコード</th>
+        <th>利用サークル数</th>
+        <th>id</th>
+        <th>サークル名</th>
+      </tr>
+    </thead>
+    <tbody>
+      <template v-for="fcode in friendsCircle">
+        <tr
+          v-for="(circle, i) in fcode.circles"
+          :key="i"
+          :class="{ different: fcode.count <= 2 }"
+        >
+          <td v-if="i === 0" :rowspan="fcode.count">{{ fcode.code }}</td>
+          <td v-if="i === 0" :rowspan="fcode.count">{{ fcode.count }}</td>
+          <td>
+            <nuxt-link
+              :to="'/kikaku/' + circle.id"
+              target="blank"
+              rel="noopener"
+              >{{ circle.id }}</nuxt-link
+            >
+          </td>
+          <td>{{ circle.circlename }}</td>
+        </tr>
+      </template>
+    </tbody>
+  </table>
+</template>
+
+<style lang="scss" scoped>
+@use '~/assets/scss/fix.scss';
+</style>
---
+++ src/pages/fix/two-circles-friend-code.vue
@@ -0,0 +1,92 @@
+<script setup lang="ts">
+useHead({
+  title: '2サークルなかよしコード処理',
+})
+
+const items = [
+  {
+    text: '「作業」セクションの「利用サークル数」が2になっているコードはありませんか？（色の塗られているセルはありませんか？）',
+    itemValue: 'セクション空',
+  },
+  {
+    text: '2サークルなかよしコード処理をスプシに転記しましたか？',
+    itemValue: 'スプシ転記',
+  },
+]
+
+const selectItems = ref([])
+</script>
+
+<template>
+  <o-article-template title="なかよしコード特殊処理">
+    <m-article-window head="作業概要">
+      <template #head>
+        <ul>
+          <li class="red bold">
+            ※二次募集を行っている場合、一次募集データ修正の段階ではこの作業を行わずに
+            <nuxt-link to="/fix/joint-adult-different"
+              >「④ーなかよしコード・合体矛盾処理（成年向け有無編）」</nuxt-link
+            >へ進む。
+          </li>
+          <li>
+            正しいなかよしコードが入力されているか確認する（文字制限は無視）。表記ゆれがあるものは植村さんの判断を仰ぐ（異なるなかよしコードの可能性がある）。
+          </li>
+          <li>
+            2サークルのみで連結しているなかよしコードを削除し、通常の合体処理に変更する（変則合体になっても構わない）。
+          </li>
+        </ul>
+      </template>
+    </m-article-window>
+    <m-article-window head="作業">
+      <template #head>
+        <m-article-paragraph secondhead="なかよしコード一覧">
+          <template #secondhead>
+            <m-friend-code-num-table />
+          </template>
+        </m-article-paragraph>
+        <m-article-paragraph secondhead="作業手順">
+          <template #secondhead>
+            <ol>
+              <li>上の表で、色のついている箇所があるか確認してください。</li>
+              <li>
+                色がついているサークルは、スプシの"friendCode"の値を削除してください。
+              </li>
+              <li>
+                2.
+                で削除したなかよしコードが2サークル間のものだった場合は、通常の合体処理に変更します。スプシの"gattainum"に合体相手の"msnum"を入れてください。
+              </li>
+            </ol>
+          </template>
+        </m-article-paragraph>
+      </template>
+    </m-article-window>
+    <m-article-window head="作業チェックリスト">
+      <template #head>
+        <div v-for="(item, i) in items" :key="i">
+          <label :for="'item' + i" class="label">
+            <input
+              :id="'item' + i"
+              v-model="selectItems"
+              type="checkbox"
+              :value="item.itemValue"
+              class="input"
+            />
+            <span class="mark"></span>
+            <span class="text">{{ item.text }}</span>
+          </label>
+        </div>
+        <a-link-button
+          v-show="items.length === selectItems.length"
+          link="/fix/joint-adult-different"
+          width="450px"
+          class="mt-12"
+          >処理④ーなかよしコード・合体矛盾処理（成年向け有無編）へ</a-link-button
+        >
+      </template>
+    </m-article-window>
+  </o-article-template>
+</template>
+
+<style lang="scss" scoped>
+@use '~/assets/scss/fix.scss';
+</style>
