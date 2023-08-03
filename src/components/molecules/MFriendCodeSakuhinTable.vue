42aee6f15e48ea0356bdb109860eeff2c536cb5d
tomoki-ls69-utl1-5 <stbwonder0725@g.ecc.u-tokyo.ac.jp>
2023-08-04 02:15:55 +0900

    合体矛盾処理（作品コード編）ページ作成

---
+++ src/components/molecules/MFriendCodeSakuhinTable.vue
@@ -0,0 +1,89 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+import { storeToRefs } from 'pinia'
+import {
+  Circle,
+  codeDifferent,
+  getSakuhin,
+  breakNewLine,
+} from '../../lib/hooks'
+import { useKikakuAllStore } from '../../store/'
+
+type Mode = 'all' | 'filtered'
+const orderMode = ref<Mode>('filtered')
+const switchOption = (mode: Mode) => {
+  orderMode.value = mode
+}
+
+const store = useKikakuAllStore()
+const { kikakuAll } = storeToRefs(store)
+const allData = kikakuAll.value as Circle[]
+const friendsCircles = codeDifferent(allData, 'sakuhincode')
+const differentFriendCircles = friendsCircles.filter(
+  circle => circle.codeDifferent === true
+)
+const showFriendCodeData = computed(() => {
+  if (orderMode.value === 'filtered') {
+    return differentFriendCircles
+  } else {
+    return friendsCircles
+  }
+})
+</script>
+
+<template>
+  <div class="mt-20">
+    <a-kikaku-radio-button
+      :isChosen="orderMode === 'all'"
+      content="全なかよしコードサークル"
+      @click="switchOption('all')"
+    />
+    <a-kikaku-radio-button
+      :isChosen="orderMode === 'filtered'"
+      content="異なるサークルのみ"
+      @click="switchOption('filtered')"
+    />
+    <table class="check-table mt-12">
+      <thead>
+        <tr>
+          <th>なかよしコード</th>
+          <th>id</th>
+          <th>サークル名</th>
+          <th>作品</th>
+          <th>補足欄内容</th>
+        </tr>
+      </thead>
+      <tbody>
+        <template v-for="(fcode, k) in showFriendCodeData" :key="k">
+          <tr
+            v-for="(circle, i) in fcode.circles"
+            :key="'friend-' + i"
+            :class="{ different: fcode.codeDifferent }"
+          >
+            <td v-if="i === 0" :rowspan="fcode.count">{{ fcode.code }}</td>
+            <td>
+              <nuxt-link :to="'/kikaku/' + circle.id">{{
+                circle.id
+              }}</nuxt-link>
+            </td>
+            <td>{{ circle.circlename }}</td>
+            <td class="code" v-html="getSakuhin(circle.sakuhincode, true)"></td>
+            <td class="hosoku" v-html="breakNewLine(circle)"></td>
+          </tr>
+        </template>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use '~/assets/scss/fix.scss';
+.hosoku {
+  max-width: 25vw;
+}
+.code {
+  overflow: hidden;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 2;
+}
+</style>
---
+++ src/pages/fix/joint-sakuhin-different.vue
@@ -0,0 +1,97 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+useHead({
+  title: 'なかよしコード・合体矛盾解消（作品コード編）',
+})
+
+const items = [
+  {
+    text: '合体・なかよしコード両方に対して作業を行いましたか？',
+    itemValue: 'セクション空',
+  },
+  {
+    text: '作品コード処理をスプシに転記しましたか？',
+    itemValue: 'スプシ転記',
+  },
+]
+const selectItems = ref([])
+</script>
+
+<template>
+  <o-article-template title="なかよしコード・合体矛盾解消（作品コード編）">
+    <m-article-window head="作業概要">
+      <template #head>
+        <p class="red bold">
+          サークル間で作品コードが異なる合体申込及びなかよしコード連結申込について、作品コードを揃える。揃え方は以下の通り。
+        </p>
+        <ul>
+          <li>
+            「配置希望の補足」欄に関係する記述があれば、基本的にはそれに合わせる
+          </li>
+          <li>
+            A（主人公）とA（主人公）以外が含まれている場合はA（主人公）以外に寄せた方がよい
+          </li>
+          <li>
+            音楽の作品コードはほぼ意味がないので、Z（その他）に寄せて構わない
+          </li>
+          <li>片方が混雑サークルであった場合はそれに寄せる</li>
+          <li>それ以外は担当者判断で構わない</li>
+        </ul>
+      </template>
+    </m-article-window>
+    <m-article-window head="作業">
+      <template #head>
+        <m-article-paragraph secondhead="作業手順">
+          <template #secondhead>
+            <ol>
+              <li>下の表で、色のついている箇所があるか確認してください。</li>
+              <li>
+                色がついているサークルは、スプシの"sakuhincode"の値を変更してください。
+              </li>
+            </ol>
+          </template>
+        </m-article-paragraph>
+        <m-article-paragraph secondhead="合体サークル">
+          <template #secondhead>
+            <m-joint-sakuhin-table />
+          </template>
+        </m-article-paragraph>
+        <m-article-paragraph secondhead="なかよしコードサークル">
+          <template #secondhead>
+            <m-friend-code-sakuhin-table />
+          </template>
+        </m-article-paragraph>
+      </template>
+    </m-article-window>
+
+    <m-article-window head="作業チェックリスト">
+      <template #head>
+        <div v-for="(item, i) in items" :key="i">
+          <label :for="'item' + i" class="label">
+            <input
+              :id="'item' + i"
+              v-model="selectItems"
+              type="checkbox"
+              :value="item.itemValue"
+              class="input"
+            />
+            <span class="mark"></span>
+            <span class="text">{{ item.text }}</span>
+          </label>
+        </div>
+        <a-link-button
+          v-show="items.length === selectItems.length"
+          link="/fix/sakuhin-media-different"
+          width="350px"
+          class="mt-12"
+          >処理⑦ー作品コード・キャラ矛盾処理</a-link-button
+        >
+      </template>
+    </m-article-window>
+  </o-article-template>
+</template>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/fix.scss';
+</style>
