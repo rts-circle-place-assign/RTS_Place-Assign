0cbf2b4a7b7cc86ec5f008d7fb15f706d87f41cd
tomoki-ls69-utl1-5 <stbwonder0725@g.ecc.u-tokyo.ac.jp>
2023-08-05 02:25:36 +0900

    申込情報集計のページ作成

---
+++ src/pages/fix/result.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import { useStudentDiscountStore } from '../../store/'
+
+useHead({
+  title: '申込情報集計',
+})
+
+const studentDiscountStore = useStudentDiscountStore()
+const { fetchStudentDiscount } = studentDiscountStore
+await fetchStudentDiscount()
+</script>
+
+<template>
+  <o-article-template title="申込情報集計">
+    <m-article-window head="概要">
+      <template #head>
+        <m-result-overview-table />
+      </template>
+    </m-article-window>
+    <m-article-window head="学割申請関連">
+      <template #head>
+        <m-article-paragraph secondhead="学校種類別">
+          <template #secondhead>
+            <m-result-student-discount-school-table />
+          </template>
+        </m-article-paragraph>
+        <m-article-paragraph secondhead="申込プラン別">
+          <template #secondhead>
+            <m-result-student-discount-plan-table />
+          </template>
+        </m-article-paragraph>
+      </template>
+    </m-article-window>
+    <m-article-window head="各種コード別サークル・スペース数">
+      <template #head>
+        <m-article-paragraph secondhead="成年向け有無別">
+          <template #secondhead>
+            <m-result-adult-table />
+          </template>
+        </m-article-paragraph>
+        <m-article-paragraph secondhead="申込プラン別">
+          <template #secondhead>
+            <m-result-plan-table />
+          </template>
+        </m-article-paragraph>
+        <m-article-paragraph secondhead="メディアコード別">
+          <template #secondhead>
+            <m-result-media-table />
+          </template>
+        </m-article-paragraph>
+        <m-article-paragraph secondhead="なかよしコード別">
+          <template #secondhead>
+            <m-result-friend-code-table />
+          </template>
+        </m-article-paragraph>
+      </template>
+    </m-article-window>
+  </o-article-template>
+</template>
