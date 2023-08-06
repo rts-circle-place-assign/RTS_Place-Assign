89556f6340a084decb8dca83216cea4dd657bf4f
tomoki-ls69-utl1-5 <stbwonder0725@g.ecc.u-tokyo.ac.jp>
2023-08-07 00:39:17 +0900

    外周抜きマスタページ作成

---
+++ src/components/molecules/MMasterTable.vue
@@ -0,0 +1,168 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { storeToRefs } from 'pinia'
+import { useKikakuAllStore } from '../../store/'
+import { Circle, PlaceAssignBaseInfo } from '../../lib/hooks'
+
+const store = useKikakuAllStore()
+const { kikakuAll } = storeToRefs(store)
+const allData = kikakuAll.value as Circle[]
+const reassigned = allData.map(circle => {
+  if (circle.hanpu1amount === undefined) {
+    return 0
+  }
+  if (circle.hanpu2amount === undefined) {
+    return 0
+  }
+  if (circle.hanpu3amount === undefined) {
+    return 0
+  }
+  if (circle.hanpu4amount === undefined) {
+    return 0
+  }
+  if (circle.hanpu5amount === undefined) {
+    return 0
+  }
+  const twitter =
+    circle.twitter === '' ? '' : 'https://twitter.com/' + circle.twitter
+  const web = circle.web === 'http://' ? '' : circle.web
+  const pixiv =
+    circle.pixiv === '' ? '' : 'https://www.pixiv.net/users/' + circle.pixiv
+  const str_base = ''
+  const zero_padding_id = ('0000' + circle.id).slice(-4)
+  const isFriendCodeCircle = circle.friendCode !== '' ? 0 : 1
+  const forSort = str_base.concat(
+    String(isFriendCodeCircle),
+    String(circle.mediacode),
+    String(circle.seijin),
+    circle.sakuhincode,
+    String(zero_padding_id)
+  )
+  // const circleHyoukaArr = hyoukaArr.find((k) => k.circlename === circle.circlename)
+  const aaa = {
+    id: circle.id,
+    msnum: circle.msnum,
+    circlename: circle.circlename,
+    penname: circle.penname,
+    sakuhincode: circle.sakuhincode,
+    mediacode: circle.mediacode,
+    seijin: circle.seijin,
+    amount:
+      Number(circle.hanpu1amount) +
+      Number(circle.hanpu2amount) +
+      Number(circle.hanpu3amount) +
+      Number(circle.hanpu4amount) +
+      Number(circle.hanpu5amount),
+    web,
+    pixiv,
+    twitter,
+    spnum: circle.spnum,
+    forSort,
+    paId: 0,
+    // best: bestHyouka.value
+  }
+  return aaa
+}) as PlaceAssignBaseInfo[]
+const jointedList = computed(() => {
+  const base = reassigned
+  base.forEach((circle, index, base) => {
+    if (circle.spnum >= 2) {
+      base.push(circle)
+    }
+  })
+  const sort = base.sort((a, b) => {
+    const smalla = a.forSort.toString().toLowerCase()
+    const smallb = b.forSort.toString().toLowerCase()
+    if (smalla < smallb) return -1
+    else if (smalla > smallb) return 1
+    return 0
+  })
+  return sort
+})
+</script>
+
+<template>
+  <table class="check-table mt-12">
+    <thead>
+      <tr>
+        <th>配置id</th>
+        <th>申込データid</th>
+        <!-- <th>MS申込番号</th> -->
+        <th>メディアコード</th>
+        <th>成年向け有無</th>
+        <th>作品コード</th>
+        <th>総搬入量</th>
+        <th class="name">サークル名</th>
+        <th class="name">ペンネーム</th>
+        <th>web</th>
+        <th>Pixiv</th>
+        <th>Twitter</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(circle, i) in jointedList" :key="i">
+        <td>{{ i + 1 }}</td>
+        <td>
+          <nuxt-link :to="'/kikaku/' + circle.id">{{ circle.id }}</nuxt-link>
+        </td>
+        <td>{{ circle.mediacode }}</td>
+        <td>{{ circle.seijin }}</td>
+        <td>{{ circle.sakuhincode }}</td>
+        <td
+          :class="{
+            'amount-mid': circle.amount >= 500 && circle.amount < 1000,
+            'amount-many': circle.amount >= 1000,
+          }"
+        >
+          {{ circle.amount }}
+        </td>
+        <td>{{ circle.circlename }}</td>
+        <td>{{ circle.penname }}</td>
+        <td>
+          <a :href="circle.web" target="_blank" rel="noopener">{{
+            circle.web
+          }}</a>
+        </td>
+        <td>
+          <a :href="circle.pixiv" target="_blank" rel="noopener">{{
+            circle.pixiv
+          }}</a>
+        </td>
+        <td>
+          <a :href="circle.twitter" target="_blank" rel="noopener">{{
+            circle.twitter
+          }}</a>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<style lang="scss" scoped>
+@use '~/assets/scss/fix.scss';
+table {
+  table-layout: fixed;
+}
+th,
+td {
+  width: 100px;
+  overflow: hidden;
+  white-space: nowrap;
+  padding: 5px;
+  max-width: 60px;
+  text-overflow: ellipsis;
+  // font-size: .9rem;
+}
+.name {
+  width: 200px;
+  max-width: 100%;
+}
+.amount {
+  &-mid {
+    background-color: #ffff00;
+  }
+  &-many {
+    background-color: #ffc000;
+  }
+}
+</style>
---
+++ src/pages/management/gaisyu-master.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts"></script>
+
+<template>
+  <o-article-template title="配置マスタデータ・外周抜き">
+    <m-article-window head="作業概要">
+      <template #head>
+        <p>
+          配置帳票に印刷するデータです。印刷のため、1SPは1つ、2SPは2つのデータが作成されています。
+        </p>
+        <p>
+          外周抜きのために、以下のデータを
+          <a
+            href="https://docs.google.com/spreadsheets/d/1E3IJQQ4BfxB2aP8A5zAqxWhofwOLUGFtJjwiHpKgPYA/edit?usp=sharingf"
+            target="_blank"
+            rel="noopener"
+            >スプシf</a
+          >にコピペしてください。
+        </p>
+      </template>
+    </m-article-window>
+    <m-article-window head="配置マスタデータ">
+      <template #head>
+        <m-master-table />
+      </template>
+    </m-article-window>
+  </o-article-template>
+</template>
+
+<style lang="scss" scoped>
+table {
+  table-layout: fixed;
+}
+th,
+td {
+  width: 100px;
+  overflow: hidden;
+  white-space: nowrap;
+  padding: 5px;
+  max-width: 60px;
+  text-overflow: ellipsis;
+  // font-size: .9rem;
+}
+.name {
+  width: 200px;
+  max-width: 100%;
+}
+.amount {
+  &-mid {
+    background-color: #ffff00;
+  }
+  &-many {
+    background-color: #ffc000;
+  }
+}
+</style>
