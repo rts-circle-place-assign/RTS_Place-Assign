6f954cfdd765b767adbe0e1dedd237a38619367c
tomoki-ls69-utl1-5 <stbwonder0725@g.ecc.u-tokyo.ac.jp>
2023-08-20 14:28:23 +0900

    MongoDBとのデータ通信部分

---
+++ src/server/database.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+
+export default async () => {
+  try {
+    mongoose.set('strictQuery', false)
+    const config = useRuntimeConfig()
+    await mongoose.connect(config.mongo.MONGO_URL)
+    console.log('Successfully connected to DB.')
+  } catch (error) {
+    return createError({
+      statusCode: 500,
+      statusMessage: 'Something went wrong.',
+    })
+  }
+}
---
+++ src/server/models/todo.model.ts
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose'
+import { ITodo } from '~/types'
+
+const schema = new mongoose.Schema<ITodo>(
+  {
+    item: {
+      type: String,
+      required: [true, 'Item field is required.'],
+      trim: true,
+      // No special characters are allowed. (upper and lower case), spaces, punctuation marks, and numbers only.
+      // validate: {
+      //     validator: function (v: string) {
+      //         return /^[a-zA-Z0-9\s\p{P}]+$/.test(v)
+      //     },
+      //     message: 'Special characters are not allowed.'
+      // },
+      // maxlength: [20, 'Max of 20 characters only.']
+    },
+    kikakuId: {
+      type: Number,
+      required: [true, 'kikakuId is required.'],
+    },
+  },
+  { timestamps: true }
+)
+
+export default mongoose.model<ITodo>('Todo', schema)
