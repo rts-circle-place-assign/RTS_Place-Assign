e98087a5f1b8e806fc46372cb415ac6262232ee8
tomoki-ls69-utl1-5 <stbwonder0725@g.ecc.u-tokyo.ac.jp>
2023-08-20 14:28:44 +0900

    MongoDBに格納するデータの型

---
+++ src/type/MongoType.ts
@@ -0,0 +1,16 @@
+import { Types } from 'mongoose'
+
+export interface ISetResponse {
+  statusCode: number
+  statusMessage: string
+  data?: any
+  cause?: any
+}
+
+export interface ITodo {
+  _id: Types.ObjectId
+  item: string
+  kikakuId: number
+  createdAt: Date
+  updatedAt: Date
+}
