37b11854144197b94a5d83d20389f7369aff3f03
tomoki-ls69-utl1-5 <stbwonder0725@g.ecc.u-tokyo.ac.jp>
2023-08-20 14:30:47 +0900

    コンポーネント表示

--- src/components/organisms/OSpecialTreatment.vue
+++ src/components/organisms/OSpecialTreatment.vue
@@ -1,8 +1,29 @@
 <script setup lang="ts">
-interface Props {
-  id: number
+import { watch, computed, reactive } from 'vue'
+import type { ISetResponse, ITodo } from '~/type'
+
+interface IAllItems extends ISetResponse {
+  data: ITodo[]
 }
-defineProps<Props>()
+
+interface Props {
+  kikakuId: number
+}
+const props = defineProps<Props>()
+
+const {
+  data: items,
+  refresh,
+  error,
+} = await useFetch<IAllItems>('/api/v1/item/all-items')
+if (error.value) {
+  throw createError(error.value)
+}
+
+const filteredItems = computed(() => {
+  const getItems = items.value.data as ITodo[]
+  return getItems.filter(item => item.kikakuId === props.kikakuId)
+})
 </script>
 
 <template>
@@ -10,7 +31,25 @@ defineProps<Props>()
     <template #head>
       <m-article-paragraph secondhead="変更・特殊対応履歴">
         <template #secondhead>
-          <a-text-area :id="id" />
+          <div class="mt-24">
+            <m-comment-form :kikakuId="kikakuId" @add-item="refresh" />
+            <ul v-if="filteredItems.length > 0" class="pl-0">
+              <m-comment-item
+                v-for="(item, i) in filteredItems"
+                :key="i"
+                :item="item"
+                @delete="refresh"
+                @update="refresh"
+              />
+            </ul>
+            <div v-else>変更・特殊対応履歴なし</div>
+            <NuxtErrorBoundary>
+              <!-- ... -->
+              <template #error>
+                <p>An error occurred: {{ error }}</p>
+              </template>
+            </NuxtErrorBoundary>
+          </div>
         </template>
       </m-article-paragraph>
     </template>
