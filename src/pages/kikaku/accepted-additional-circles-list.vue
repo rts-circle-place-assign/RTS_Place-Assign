23c372831b3f69dd08e0124fb150131b1e5dccd9
tomoki-ls69-utl1-5 <stbwonder0725@g.ecc.u-tokyo.ac.jp>
2023-08-28 04:41:37 +0900

    「二次募集受付サークル一覧」ページ作成

---
+++ src/pages/kikaku/accepted-additional-circles-list.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+useHead({
+  title: '二次募集受付サークル一覧',
+})
+</script>
+
+<template>
+  <o-article-template title="二次募集受付サークル一覧">
+    <m-accepted-circles-table :isSecond="true" />
+  </o-article-template>
+</template>
