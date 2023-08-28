82f21e3062276eaacf1c2ce1517414be4cdd00c3
tomoki-ls69-utl1-5 <stbwonder0725@g.ecc.u-tokyo.ac.jp>
2023-08-29 02:03:40 +0900

    「配置作業管理トップ」ページ作成

---
+++ src/pages/management/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useHead } from '@unhead/vue'
+
+useHead({
+  title: '配置作業管理トップ',
+})
+</script>
+
+<template>
+  <o-article-template title="配置作業管理トップ">
+    <m-article-window head="配置作業準備日の流れ">
+      <template #head>
+        <m-place-assign-flow />
+      </template>
+    </m-article-window>
+  </o-article-template>
+</template>
