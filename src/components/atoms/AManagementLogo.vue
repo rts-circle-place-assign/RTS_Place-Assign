6914af2a5a9c63f4298a4dd34a7e7b918ad4cae3
tomoki-ls69-utl1-5 <stbwonder0725@g.ecc.u-tokyo.ac.jp>
2023-08-29 01:53:29 +0900

    頭紙にロゴを挿入

---
+++ src/components/atoms/AManagementLogo.vue
@@ -0,0 +1,11 @@
+<template>
+  <img
+    src="~/assets/img/template/arts10_mono1_white-based.png"
+    alt="ARTS10ロゴ"
+    class="logo"
+  />
+</template>
+
+<style lang="scss" scoped>
+@use '~/assets/scss/management.scss';
+</style>
