ed027ea0a676e556a5fff7acd62f64b48d80c539
tomoki-ls69-utl1-5 <stbwonder0725@g.ecc.u-tokyo.ac.jp>
2023-08-29 02:03:59 +0900

    ページ名変更

--- src/pages/fix/index.vue
+++ src/pages/fix/index.vue
@@ -2,12 +2,12 @@
 import { useHead } from '@unhead/vue'
 
 useHead({
-  title: 'サークル申込データ修正',
+  title: 'サークル申込データ修正トップ',
 })
 </script>
 
 <template>
-  <o-article-template title="サークル申込データ修正">
+  <o-article-template title="サークル申込データ修正トップ">
     <div class="flex justify-center mx-auto">
       <a-link-button class="mb-20" link="/fix/result"
         >修正結果集計</a-link-button
