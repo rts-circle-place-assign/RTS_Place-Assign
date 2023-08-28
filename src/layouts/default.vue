68dab69f749c9135a28beb96618d5904cb99cf24
tomoki-ls69-utl1-5 <stbwonder0725@g.ecc.u-tokyo.ac.jp>
2023-08-29 02:03:08 +0900

    default.vueに印刷時のcssを追加

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -36,3 +36,26 @@ const hasSearchMenu = computed(() => !route.path.match(/\/search\/?$/))
   }
 }
 </style>
+<style lang="scss">
+@media print {
+  html {
+    -ms-text-size-adjust: 100%;
+    -webkit-text-size-adjust: 100%;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    box-sizing: border-box;
+    @media print {
+      @page {
+        size: A4;
+      }
+    }
+  }
+
+  *,
+  *:before,
+  *:after {
+    box-sizing: border-box;
+    margin: 0;
+  }
+}
+</style>
