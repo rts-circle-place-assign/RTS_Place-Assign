b8b49d24a74042f39aab27eec0c960aedf391b31
tomoki-ls69-utl1-5 <stbwonder0725@g.ecc.u-tokyo.ac.jp>
2023-08-29 23:46:17 +0900

    こちらも変更

--- src/pages/management/media.vue
+++ src/pages/management/media.vue
@@ -29,6 +29,25 @@ useHead({
           <li>
             それぞれのカゴに入っている短冊の枚数を数えて1紙に書き、「理論SP数」と一致するか確認する。一致しなければ再カウントし、それでも一致しなければながたへ。
           </li>
+          <li>
+            植村さんが外周抜きのスプシに入力し終えたらスプシを複数部印刷する。
+            <a-link-button
+              link="https://docs.google.com/spreadsheets/d/1E3IJQQ4BfxB2aP8A5zAqxWhofwOLUGFtJjwiHpKgPYA/edit#gid=2076896089"
+              target="_blank"
+              rel="noopener"
+              >外周抜きスプシ</a-link-button
+            >
+          </li>
+          <li>
+            外周抜きスプシの「今期最高評価」に対応する付箋を短冊に貼る。
+            <a-sticky-note-color />
+          </li>
+          <li>
+            ここまで無事に終了したら「2.5. 作品コードソート」へ
+            <a-link-button link="/management/sakuhin" class="m-10"
+              >2.5. 作品コードソート</a-link-button
+            >
+          </li>
         </ol>
       </template>
     </m-article-window>
