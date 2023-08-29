a27c2fcc69f908dc03c28bad6bd8f0b87e197eaa
tomoki-ls69-utl1-5 <stbwonder0725@g.ecc.u-tokyo.ac.jp>
2023-08-29 23:46:02 +0900

    付箋の色の表

---
+++ src/components/atoms/AStickyNoteColor.vue
@@ -0,0 +1,28 @@
+<template>
+  <table class="check-table">
+    <thead>
+      <tr>
+        <th>評価</th>
+        <th>色</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>A</td>
+        <td>ピンク</td>
+      </tr>
+      <tr>
+        <td>B</td>
+        <td>オレンジ</td>
+      </tr>
+      <tr>
+        <td>C</td>
+        <td>イエロー</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<style lang="scss" scoped>
+@use '~/assets/scss/fix.scss';
+</style>
