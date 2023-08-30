860b962687cb6ad6ba0fd292bcaf193112b0c271
tomoki-ls69-utl1-5 <stbwonder0725@g.ecc.u-tokyo.ac.jp>
2023-08-30 11:38:51 +0900

    Excelファイル数値入力の手順を追加

---
+++ src/pages/management/gaisyu-adjust-excel.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { useHead } from '@unhead/vue'
+
+useHead({
+  title: 'Excelファイル初期設定',
+})
+</script>
+
+<template>
+  <o-article-template title="Excelファイル初期設定">
+    <m-article-window head="作業の流れ">
+      <template #head>
+        <ol>
+          <li>
+            理論SP数・短冊カウントを比較するExcelファイルを開く。
+            <m-excel-download />
+          </li>
+          <li>
+            Excelファイル「2.5紙-理論SP数・付箋有無合計SP数比較」シートを開く。
+          </li>
+          <li>「理論SP」の列に以下の表の数値を入力する。</li>
+        </ol>
+      </template>
+    </m-article-window>
+  </o-article-template>
+</template>
