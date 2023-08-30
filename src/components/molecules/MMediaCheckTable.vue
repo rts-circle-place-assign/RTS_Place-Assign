2ec714f2f928b0d0a27ef21efc7bc7f171e620d4
tomoki-ls69-utl1-5 <stbwonder0725@g.ecc.u-tokyo.ac.jp>
2023-08-30 11:35:56 +0900

    1紙チェック欄追加

--- src/components/molecules/MMediaCheckTable.vue
+++ src/components/molecules/MMediaCheckTable.vue
@@ -4,12 +4,18 @@
       <th colspan="2">担当者サイン</th>
     </tr>
     <tr>
-      <th>落選等<br />特殊対応</th>
+      <th>申込情報変更<br />落選等<br />特殊対応</th>
+      <td class="input name"></td>
+    </tr>
+    <tr>
+      <th>短冊カウント</th>
       <td class="input name"></td>
     </tr>
     <tr>
       <!-- <td class="input gaisyu-end" rowspan="2"></td> -->
       <th>2SP・合体<br />ホチキス</th>
+      <td class="input name"></td>
+      <th>なかよし<br />クリップ</th>
       <td class="input name"></td>
     </tr>
     <tr>
