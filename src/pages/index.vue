63c7245d9ab68db7600030452aa0e6d6f755ef7c
tomoki-ls69-utl1-5 <stbwonder0725@g.ecc.u-tokyo.ac.jp>
2023-08-31 15:03:51 +0900

    不要なので削除

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,13 +4,6 @@ import { useHead } from '@unhead/vue'
 useHead({
   title: 'トップページ',
 })
-
-// const { data: thisPlaceAssignData } = await useFetch('/api/thisPlaceAssign', {
-//   key: 'thisPlaceAssignData',
-// })
-// const { data: pastPlaceAssignData } = await useFetch('/api/pastPlaceAssign.get', {key: 'pastPlaceAssignData'})
-// const { data: studentDiscountData } = await useFetch('/api/studentDiscount', {key: 'studentDiscountData'})
-// const { data: pastGaisyuData } = await useFetch('/api/pastGaisyu', {key: 'pastGaisyuData'})
 </script>
 
 <template>
