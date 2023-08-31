defc7fd3a4ed5ec77469039d35c632df9d266440
tomoki-ls69-utl1-5 <stbwonder0725@g.ecc.u-tokyo.ac.jp>
2023-08-31 22:14:54 +0900

    ロゴのheightをpropsで渡せるようにした

--- src/components/atoms/AManagementLogo.vue
+++ src/components/atoms/AManagementLogo.vue
@@ -1,13 +1,18 @@
+<script setup lang="ts">
+interface Props {
+  height?: string
+}
+
+withDefaults(defineProps<Props>(), {
+  height: '10vh',
+})
+</script>
+
 <template>
   <img
     src="~/assets/img/template/arts10_mono1_white-based.png"
     alt="ARTS10ロゴ"
     class="logo"
+    :style="{ height: height }"
   />
 </template>
-
-<style lang="scss" scoped>
-.logo {
-  height: 10vh;
-}
-</style>
