1e7b711eb367140d183332ebd50ad35ed6f4a33c
tomoki-ls69-utl1-5 <stbwonder0725@g.ecc.u-tokyo.ac.jp>
2023-09-17 16:03:16 +0900

    使用されていないtypeを削除

--- src/type/CircleType.ts
+++ src/type/CircleType.ts
@@ -8,17 +8,17 @@ export interface PlaceAssign {
   penname: string
 }
 
-export interface CirclePAData {
-  kaiki: string
-  space: string
-  gaisyu: string
+export interface ThisPlaceAssign extends PlaceAssign {
+  circlenamekana: string
+  pennamekana: string
+  twitter: string
+  pixiv: string
+  web: string
 }
 
 export interface Circle {
   id: number
   cutId?: string
-  cutwidth: number
-  cutheight: number
   circlename: string
   circlenamekana: string
   gattainum?: string
@@ -29,7 +29,7 @@ export interface Circle {
   mediacode: number
   seijin: number
   hosoku?: string
-  web?: string
+  web: string
   webok: number
   pixiv?: string
   pixivok: number
@@ -61,12 +61,6 @@ export interface Circle {
   hanpu5date?: number
   hanpu5price?: number
   hanpu5amount?: number
-  phone?: string
-  doujinhanbai?: string
-  purpose?: string
-  funold?: string
-  youbou?: string
-  uketukenum?: number
   msnum?: string
   spnum: number
   twitterfollownum?: number
@@ -82,122 +76,10 @@ export interface Circle {
   code: string // RTS20学割照合用コード
 }
 
-export interface GattaiProcess {
-  midashi?: string
-  thiscricle?: string
-}
-
-export interface HanpuData {
-  name: string
-  ProcessedDate: number | string
-  ProcessedPrice: number | string
-  ProcessedAmount: number | string
-}
-
-export interface SpaceSet {
-  num: number
-  space: string
-}
-export interface GtSet {
-  midashi: string
-  key: string
-}
-
-export interface SakuhinSet {
-  code: string
-  sakuhin: string
-}
-
-export interface MediaSet {
-  code: number
-  media: string
-}
-
-export interface Tanseki {
-  num: number[]
-  block: string[]
-}
-
-export interface Gaisyu {
-  kaiki: string
-  tanseki: Tanseki[]
-  kabeBlock: string[]
-}
-
-export interface JointInfo {
-  midashi: string
-  key: string
-}
-
-export interface CutInfo {
-  width: number
-  height: number
-}
-
-export interface FriendCodeInfo {
-  code: string
-  count: number
-  circles: Circle[]
-  different: boolean
-  codeDifferent: boolean
-}
-
-export interface CircleMinInfo {
-  id: number
-  circlename: string
-  penname: string
-  mediacode: number
-  sakuhincode: string
-  seijin: number
-}
-
-export interface PlaceAssignBaseInfo {
-  id: number
-  circlename: string
-  penname: string
-  sakuhincode: string
-  mediacode: number
-  seijin: number
-  amount: number
-  web: string | undefined
-  pixiv: string | undefined
-  twitter: string | undefined
-  spnum: number
-  forSort: string
-}
-
 export interface PastGaisyuInfo {
   circlename: string
   best: string
   konzatsu: string
-}
-
-export interface SortedSakuhinForCount {
-  id: string
-  sakuhincode: string | undefined
-  sakuhin: string
-  mediacode: number
-  media: string
-  adultNum: number
-  spNum: number
-  forSort: string
-  pink: number
-  orange: number
-  yellow: number
-  green: number
-  other: number
-  adjust: number
-  jiko: number
-}
-
-export interface SortedSakuhinForRTDB {
-  [key: string]: SortedSakuhinForCount
-}
-
-export interface CatalogInfo {
-  rank: number
-  space: string
-  circlename: string
 }
 
 export interface StudentDiscountData {
