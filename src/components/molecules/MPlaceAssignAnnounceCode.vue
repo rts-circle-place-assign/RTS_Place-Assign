551cdc28f7939268565e68f5f7c88fe2f6a86a68
tomoki-ls69-utl1-5 <stbwonder0725@g.ecc.u-tokyo.ac.jp>
2023-09-22 01:24:10 +0900

    HTMLコード部分をコンポーネントとしてまとめた

---
+++ src/components/molecules/MPlaceAssignAnnounceCode.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <client-only>
+    <lazy-a-place-assign-announce-base-code-head />
+    <lazy-a-place-assign-json />
+    <lazy-a-place-assign-announce-base-code-bottom />
+  </client-only>
+</template>
+
+<style scoped lang="scss"></style>
