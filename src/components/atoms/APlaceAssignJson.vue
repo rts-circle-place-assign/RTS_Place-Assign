712a8c2f6e4c423d43108245c399694052ea306c
tomoki-ls69-utl1-5 <stbwonder0725@g.ecc.u-tokyo.ac.jp>
2023-09-22 01:24:56 +0900

    utilsに括りだした

--- src/components/atoms/APlaceAssignJson.vue
+++ src/components/atoms/APlaceAssignJson.vue
@@ -1,20 +1,7 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
+import { toArrayObjects } from '~/lib/utils/array-utils'
 
-const toArrayObjects = (table: HTMLElement) => {
-  const columns = Array.from(table.querySelectorAll('th')).map(heading => heading.textContent)
-
-  const rows = table.querySelectorAll('tbody > tr')
-
-  return Array.from(rows).map(row => {
-    const dataCells = Array.from(row.querySelectorAll('td'))
-
-    return columns.reduce((obj, column, index) => {
-      obj[column] = dataCells[index].textContent
-      return obj
-    }, {})
-  })
-}
 const text = ref('')
 onMounted(() => {
   const data = document.getElementById('place-assign-table')!
