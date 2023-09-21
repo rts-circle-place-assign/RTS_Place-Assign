a3c98a6620c93faabb8000f2dc21f001626c695d
tomoki-ls69-utl1-5 <stbwonder0725@g.ecc.u-tokyo.ac.jp>
2023-09-22 01:02:41 +0900

    table要素をobjにする関数

--- src/lib/utils/array-utils.ts
+++ src/lib/utils/array-utils.ts
@@ -13,3 +13,17 @@ export const shuffle = <T>(arr: T[]): T[] => {
   }
   return copy
 }
+
+// table要素をObj[]にする関数
+export const toArrayObjects = (table: HTMLElement) => {
+  const columns = Array.from(table.querySelectorAll('th')).map(heading => heading.textContent)
+  const rows = table.querySelectorAll('tbody > tr')
+  return Array.from(rows).map(row => {
+    const dataCells = Array.from(row.querySelectorAll('td'))
+    return columns.reduce((obj, column, index) => {
+      // @ts-ignore
+      obj[column!] = dataCells[index].textContent
+      return obj
+    }, {})
+  })
+}
