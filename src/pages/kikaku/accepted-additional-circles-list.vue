5d713d87150f3c1f21905f4d195dedc5f58e6441
Tomoki NAGATA <stbwonder0725@g.ecc.u-tokyo.ac.jp>
2024-02-10 11:50:34 +0900

    二次募集受付サークル一覧ページにもページ作成手順を掲載

--- src/pages/kikaku/accepted-additional-circles-list.vue
+++ src/pages/kikaku/accepted-additional-circles-list.vue
@@ -8,6 +8,20 @@ useHead({
 
 <template>
   <o-article-template title="二次募集受付サークル一覧">
-    <lazy-m-accepted-circles-table :isSecond="true" />
+    <m-article-window head="ページ作成手順">
+      <template #head>
+        <ol>
+          <li>
+            以下のHTMLコード3つを順番にWordPressのHTMLコード部分にコピペする。
+            <m-accepted-circles-code />
+          </li>
+          <li>
+            以下のCSSをWordPressのHTMLコード部分に貼り付ける。
+            <a-accepted-circles-list-css />
+          </li>
+        </ol>
+      </template>
+    </m-article-window>
+    <lazy-o-accepted-circles-list :isSecond="true" />
   </o-article-template>
 </template>
