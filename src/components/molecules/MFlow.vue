7cabf8de53ab2b73cda9c48be0e0be69faf39f66
tomoki-ls69-utl1-5 <stbwonder0725@g.ecc.u-tokyo.ac.jp>
2024-11-11 19:19:11 +0900

    流れを表す部分をMFlow.vueとしてコンポーネント化

---
+++ src/components/molecules/MFlow.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+interface Flow {
+  title: string
+  link: string
+}
+
+interface Props {
+  flows: Flow[]
+  dir?: string
+}
+
+withDefaults(defineProps<Props>(), {
+  dir: '',
+})
+</script>
+
+<template>
+  <div class="container">
+    <a-timeline-box
+      v-for="(page, i) in flows"
+      :key="i"
+      :head="page.title"
+      :link="page.link === '' ? '' : `/${dir}/${page.link}`"
+    />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  max-width: 1024px;
+  width: 100%;
+  @include mq(from-pc) {
+    margin: 0 auto;
+  }
+  margin: 0 auto;
+  @include mq(until-tab) {
+    margin-left: 0.5rem;
+  }
+}
+</style>
---
+++ src/components/organisms/OBeforeFixFlow.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const pages = [
+  {
+    title: '新規作品コード追加',
+    link: 'new-sakuhin-code',
+  },
+]
+</script>
+
+<template>
+  <m-flow :flows="pages" dir="before-fix" />
+</template>
