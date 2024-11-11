e412267dd69ebf06584df347db6459eaff6fbfa8
tomoki-ls69-utl1-5 <stbwonder0725@g.ecc.u-tokyo.ac.jp>
2024-11-11 22:57:56 +0900

    SRTS2の情報に変更

--- src/composables/useKaikiStore.ts
+++ src/composables/useKaikiStore.ts
@@ -2,14 +2,13 @@ type kaikiStore = {
   theDay: number
   kaikiJa: string
   kaikiEn: string
-  kaikiEn: string
 }
 
 export const useKaikiStore = () => {
   const state = useState<kaikiStore>('kaiki', () => ({
-    theDay: 20241020,
-    kaikiJa: '第十一回博麗神社秋季例大祭',
-    kaikiEn: 'ATS11',
+    theDay: 20250323,
+    kaikiJa: '第二回博麗神社例大祭 in 静岡',
+    kaikiEn: 'SRTS2',
   }))
   return {
     state: readonly(state),
