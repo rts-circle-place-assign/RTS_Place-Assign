7963f17288661b1e3f525f1e38d14b81de8177e7
tomoki-ls69-utl1-5 <stbwonder0725@g.ecc.u-tokyo.ac.jp>
2024-11-17 13:03:30 +0900

    学割申請データ入力の手順を追加

--- src/pages/before-fix/student-discount-application.vue
+++ src/pages/before-fix/student-discount-application.vue
@@ -1,5 +1,77 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useHead } from '@unhead/vue'
 
-<template></template>
+useHead({
+  title: '学割申請データ入力',
+})
+
+const items = [
+  {
+    text: '「store格納情報」の表の内容は今期のものになっていますか？',
+    itemValue: 'store格納情報',
+  },
+]
+</script>
+
+<template>
+  <o-article-template title="学割申請データ入力">
+    <m-article-window head="作業概要">
+      <template #head>
+        <p>今期の申込情報を格納するスプシに、学割申請情報を格納する。</p>
+      </template>
+    </m-article-window>
+    <m-article-window head="作業">
+      <template #head>
+        <m-article-paragraph secondhead="作業手順">
+          <template #secondhead>
+            <ol>
+              <li>
+                <a
+                  href="https://docs.google.com/spreadsheets/d/1lw1a1RqQ2Kdszu8MUBWsSzagO24WStVWNoGjKNCHXJc/edit?gid=1262282731#gid=1262282731"
+                  target="_blank"
+                  rel="noopener"
+                  >学割申請フォーム回答スプシ</a
+                >を開く。
+              </li>
+              <li>「お名前（本名）」「サークル名」の列をコピーする。</li>
+              <li>
+                <a
+                  href="https://docs.google.com/spreadsheets/d/1OlBhlndoJBG7WW-zCYI-yr9ceJAOYpK9D66pHdsatK8/edit?gid=1156355093#gid=1156355093"
+                  target="_blank"
+                  rel="noopener"
+                  >サークル申込情報スプシ</a
+                >を開く。
+              </li>
+              <li>「(kaiki)_studentDiscount」シートを開き、2.でコピーしたものを貼り付ける。</li>
+              <li>
+                <a
+                  href="https://script.google.com/home/projects/1P2wwWu7pXPVoek23MivVcrm2wDubH9fmuvLYn24Y-7p4Ob7KJR8MCsOG/edit"
+                  target="_blank"
+                  rel="noopener"
+                ></a
+                >学割申請情報取得GASを開く。
+              </li>
+              <li>
+                「デプロイ」>「新しいデプロイ」を押し、以下の設定で「デプロイ」を押す。
+                <ul>
+                  <li>説明：会期英略称</li>
+                  <li>次のユーザーとして実行：自分（circle.place.assign@gmail.com）</li>
+                  <li>アクセスできるユーザー：全員</li>
+                </ul>
+              </li>
+              <li>表示されたURLをコピーして、.envのGAS_STUDENTDISCOUNT_APPLICATION_URLに貼り付ける。</li>
+            </ol>
+          </template>
+        </m-article-paragraph>
+        <m-check-list
+          :items="items"
+          link="/before-fix/student-discount-application"
+          width="350"
+          next="学割申請データ入力へ"
+        />
+      </template>
+    </m-article-window>
+  </o-article-template>
+</template>
 
 <style scoped lang="scss"></style>
