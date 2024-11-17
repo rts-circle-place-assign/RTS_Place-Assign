d5b72c7187c147692e43b695df2110ca6b8d5d11
tomoki-ls69-utl1-5 <stbwonder0725@g.ecc.u-tokyo.ac.jp>
2024-11-17 21:34:11 +0900

    ダウンロードボタンをコンポーネント化

---
+++ src/components/atoms/ADownloadButton.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+interface Props {
+  fileName: string
+  downloadName?: string
+  name: string
+  width: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  downloadName: '',
+})
+
+const download = () => {
+  const link = document.createElement('a')
+  link.href = `/${props.fileName}`
+  link.download = props.downloadName ? props.downloadName : props.fileName
+  link.target = '_blank'
+  link.click()
+}
+</script>
+
+<template>
+  <a-link-button :width="width" class="download-button" @click="download">{{ name }}ダウンロード</a-link-button>
+</template>
+
+<style scoped lang="scss">
+.download-button {
+  cursor: pointer;
+}
+</style>
